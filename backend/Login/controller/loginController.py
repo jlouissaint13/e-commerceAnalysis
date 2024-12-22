c68827b02ea5e91dddd9f5f0de0b5cc2a3e31169
jlouissaint13 <jaredlouissaint@gmail.com>
2024-12-22 13:53:10 -0500

    added all labels to Creation.jsx

---
+++ backend/Login/controller/loginController.py
@@ -0,0 +1,22 @@
+from flask import Flask,Blueprint, render_template, jsonify, request
+from sqlalchemy import false
+
+from backend.Login.model import loginModel
+loginBlueprint = Blueprint('login',__name__)
+LoginModel = loginModel.LoginModel
+# authStatus = False
+
+@loginBlueprint.route('/submit',methods=['POST'])
+def receiveLogin():
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+    print(username,password)
+    loginModel1 = LoginModel(username,password)
+    if (loginModel1.loginUser()):
+
+        return jsonify({"message": "Valid account continue"}), 200
+    return jsonify({"message": "Failure to authenticate"}), 401
+
+
