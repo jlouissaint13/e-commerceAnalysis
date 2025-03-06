df1307765f89a98272890c900c58f7b7e1246063
jared <jaredlouissaint@gmail.com>
2025-03-06 13:54:17 -0500

    Small fix

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
            <BrowserRouter>
                <Routes>
-                   <Route index={<Login/>} />
+                   <Route index element={<Login/>} />
                    <Route path="/Login" element={<Login/>}/>
                    <Route path="/Creation" element={<Creation/>}/>
                    <Route path={'/Data'} element={<DataPage/>}/>
